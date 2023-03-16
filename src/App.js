1e2e94106db4182cc2cb89667ccf1fe513432249
Muhammed-Bilal777 <94797862+Muhammed-Bilal777@users.noreply.github.com>
2023-03-16 22:38:37 +0530

    checking project is running or not

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 
 import { Route, Routes } from 'react-router-dom'
-import './App.scss';
 import Home from './components/Home'
 import Layout from './components/Layout/'
+import './App.scss';
 
 function App() {
   return (
-     <>  
+     <>    <h1>checking why components are not rendering</h1>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
-            <h1>checking why components are not rendering</h1>
+           
             {/* <Route path="about" element={<About />} />
             <Route path="/contact" element={<Contact />} /> */}
           </Route>
