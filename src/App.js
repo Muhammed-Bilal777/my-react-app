bc2fe5f13c91beb78db1c0f24ce7729d21087122
Muhammed-Bilal777 <94797862+Muhammed-Bilal777@users.noreply.github.com>
2023-03-16 22:32:20 +0530

    checking project is running or not

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
+            <h1>checking why components are not rendering</h1>
             {/* <Route path="about" element={<About />} />
             <Route path="/contact" element={<Contact />} /> */}
           </Route>
