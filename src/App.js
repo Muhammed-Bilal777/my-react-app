f7a1c374424eb472640e6ea59937b0318e7b24fc
Muhammed-Bilal777 <94797862+Muhammed-Bilal777@users.noreply.github.com>
2023-03-16 19:09:01 +0530

    Added @emailjs/library

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Lovely Working fine everything
+         Added Emailjs/browser library to check
         </p>
         <a
           className="App-link"
