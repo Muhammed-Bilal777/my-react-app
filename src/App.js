739867b67f7c2a7b3f1a919ff80052e1cd2a7e4a
Muhammed-Bilal777 <94797862+Muhammed-Bilal777@users.noreply.github.com>
2023-03-19 02:38:51 +0530

    finished  project

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './components/Home'
-import Layout from './components/Layout/'
-import './App.scss';
-import Sidebar from './components/Sidebar';
-import Contact from './components/Contact'
-import About from './components/About'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Layout from "./components/Layout/";
+import "./App.scss";
+import Sidebar from "./components/Sidebar";
+import Contact from "./components/Contact";
+import About from "./components/About";
 
 function App() {
   return (
-     <>    
-     <Router>
-          <Layout />
-          <Sidebar />
+    <>
+      <Router>
+        <Layout />
+        <Sidebar />
+
+        <Routes>
            
-        <Routes >
+            <Route path="/" element={<Home />} />
+            <Route path="/about/" element={<About />} />
+            <Route path="/contact/" element={<Contact />} />
            
-          <Route  >
-              
-            <Route path='/' element={<Home />} />
-            <Route path='/about/' element={<About />} />
-            <Route path='/contact/' element={<Contact />} />
-            
-          </Route>
-       </Routes>  
-       </Router>   
-     </>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
